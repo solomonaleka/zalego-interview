52f02e92e323e2eae2a88e27e084d3a8e9e20e59
james ngatia <james.ngatia@turnkeyafrica.com>
2018-10-24 18:14:06 +0300

    hhhhh

--- welcome.php
+++ welcome.php
@@ -7,4 +7,10 @@ echo "<h1>Welcome ".$_SESSION['username']." you have logged in successfully!!<h1
 
 session_start();
 echo "<h1>Welcome ".$_SESSION['username']." you have logged in successfully!!<h1 />";
+
+session_start();
+echo "<h1>Welcome ".$_SESSION['username']." you have logged in successfully!!<h1 />";
+
+session_start();
+echo "<h1>Welcome ".$_SESSION['username']." you have logged in successfully!!<h1 />";
 ?>
