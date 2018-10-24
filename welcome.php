d379620c68264e1eb58878223912bd0c0275e3ca
james ngatia <james.ngatia@turnkeyafrica.com>
2018-10-24 18:03:40 +0300

    Changes on develop2

--- welcome.php
+++ welcome.php
@@ -7,4 +7,8 @@ echo "<h1>Welcome ".$_SESSION['username']." you have logged in successfully!!<h1
 
 session_start();
 echo "<h1>Welcome ".$_SESSION['username']." you have logged in successfully!!<h1 />";
+session_start();
+echo "<h1>Welcome ".$_SESSION['username']." you have logged in successfully!!<h1 />";
+session_start();
+echo "<h1>Welcome ".$_SESSION['username']." you have logged in successfully!!<h1 />";
 ?>
