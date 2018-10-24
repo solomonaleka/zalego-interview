51ad09d17db8764752e5a9d72a20527a83738212
james ngatia <james.ngatia@turnkeyafrica.com>
2018-10-24 17:50:07 +0300

    Changes on welcome.php

--- welcome.php
+++ welcome.php
@@ -1,4 +1,7 @@
 <?php
 session_start();
 echo "<h1>Welcome ".$_SESSION['username']." you have logged in successfully!!<h1 />";
+
+session_start();
+echo "<h1>Welcome ".$_SESSION['username']." you have logged in successfully!!<h1 />";
 ?>
