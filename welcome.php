446fe1aa6e438ec73768fd2f6e572859c3d84263
james ngatia <james.ngatia@turnkeyafrica.com>
2018-10-24 17:57:13 +0300

    New changes to develop branch

--- welcome.php
+++ welcome.php
@@ -1,4 +1,10 @@
 <?php
 session_start();
 echo "<h1>Welcome ".$_SESSION['username']." you have logged in successfully!!<h1 />";
+
+session_start();
+echo "<h1>Welcome ".$_SESSION['username']." you have logged in successfully!!<h1 />";
+
+session_start();
+echo "<h1>Welcome ".$_SESSION['username']." you have logged in successfully!!<h1 />";
 ?>
